4ab39156aa82137b456d030137b972f3b06194ea
karelinas <45606302+karelinas@users.noreply.github.com>
2018-12-09 22:12:31 +0200

    Optimize day5 by reducing before problem unit search

--- day05/solution.cpp
+++ day05/solution.cpp
@@ -3,11 +3,11 @@
 #include <algorithm>
 #include <cctype>
 #include <cstddef>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <limits>
 #include <list>
-#include <stack>
 #include <type_traits>
 
 std::list<char> read_polymer(std::string file)
@@ -28,30 +28,32 @@ bool reacts(char lunit, char runit)
     return std::abs(lunit - runit) == ('a' - 'A');
 }
 
-int reduced_length(const std::list<char>& polymer)
+template <typename  List>
+List reduce(const List& polymer)
 {
-    std::stack<char> reduced;
+    std::deque<char> reduced;
     for (auto unit : polymer) {
-        if (reduced.size() > 0 && reacts(unit, reduced.top())) {
-            reduced.pop();
+        if (reduced.size() > 0 && reacts(unit, reduced.front())) {
+            reduced.pop_front();
         } else {
-            reduced.push(unit);
+            reduced.push_front(unit);
         }
     }
-    return reduced.size();
+
+    return List{std::begin(reduced), std::end(reduced)};
 }
 
 int reduced_length_without(const std::list<char>& polymer, char without)
 {
-    std::stack<char> reduced;
+    std::deque<char> reduced;
     for (auto unit : polymer) {
         if (std::toupper(unit) == std::toupper(without)) {
             continue;
         }
-        if (reduced.size() > 0 && reacts(unit, reduced.top())) {
-            reduced.pop();
+        if (reduced.size() > 0 && reacts(unit, reduced.front())) {
+            reduced.pop_front();
         } else {
-            reduced.push(unit);
+            reduced.push_front(unit);
         }
     }
     return reduced.size();
@@ -60,14 +62,14 @@ int reduced_length_without(const std::list<char>& polymer, char without)
 int main()
 {
     auto polymer = read_polymer("data.txt");
-
+    auto reduced = reduce(polymer);
     // part 1
-    std::cout << "Reduced polymer length: " << reduced_length(polymer) << '\n';
+    std::cout << "Reduced polymer length: " << reduced.size() << '\n';
 
     // part 2
     int min_length = std::numeric_limits<int>::max();
     for (char unit = 'a'; unit <= 'z'; unit++) {
-        int length = reduced_length_without(polymer, unit);
+        int length = reduced_length_without(reduced, unit);
         if (length < min_length) min_length = length;
     }
     std::cout << "Shortest reduced polymer after problem removal: " << min_length << '\n';
